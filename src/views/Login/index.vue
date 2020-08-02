d2589e471648c0e2c5be7a37fcb79bbfb5b975db
lxlw1320@gmail.com <lxlw1320@gmail.com>
2020-08-02 14:27:15 +0800

    init-0.1

---
+++ src/views/Login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="login">
+    <el-button>测试一下</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {}
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
+
